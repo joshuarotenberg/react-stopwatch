1abe7fa4c417b247675dc622fde63863ed94e354
Josh Rotenberg <joshua.rotenberg@gmail.com>
2021-03-24 20:56:09 -0400

    adding stopwatch app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [timer, setTimer] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+
+  const handleReset = () => {
+    setTimer(false)
+    setSeconds(0)
+    secondHand.style.transform = `rotate(90deg)`;
+  }
+
+  
+  const handleStart = () => setTimer(prevTimer => !prevTimer);
+  const timer_started_message = timer ? 'Stop Timer' : 'Start Timer';
+  const secondHand = document.querySelector('.second-hand');
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+
+  useEffect(() => {
+    let clock = null;
+    if (timer) {
+      clock = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+      }, 1000);
+    } 
+    return () => {
+      clearInterval(clock);
+    }
+  }, [timer, seconds])
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <div class="clock">
+            <div class="clock-face">
+              <div class="hand second-hand"></div>
+              <div className="circle">
+              <div className="time">{seconds}</div>
+              </div>
+            </div>
+        </div>
+        <div>
+          <button onClick={handleStart}>{timer_started_message}</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
